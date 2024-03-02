0d1080b19d5228a04e61823c3d8318a83d9f3de5
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-03-01 21:40:35 -0800

    Create AddTodoForm component

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,11 @@
+const AddTodoForm = () => {
+    return (
+        <form>
+            <label htmlFor="todoTitle">Title</label>
+            <input id="todoTitle" type="text" />
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default AddTodoForm
