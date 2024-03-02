0b4ac87631a27dbb0211dd8d52cb04afe1ce345a
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-03-01 21:44:42 -0800

    Add AddTodoForm to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TodoList from './TodoList.jsx'
+import AddTodoForm from './AddTodoForm.jsx'
 
 const App = () => {
   return (
     <>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </>
   )
