61f7a84cd560b5a811808a4b27277257fc3c179f
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-03-20 18:44:29 -0700

    lesson 1.4

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,17 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 const AddTodoForm = (props) => {
+  const { onAddTodo } = props;
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
+
   const handleAddTodo = (event) => {
     event.preventDefault();
-    const todoTitle = event.target.title.value;
     console.log(todoTitle);
-    event.target.reset();
-    props.onAddTodo(todoTitle);
+    setTodoTitle("");
+    onAddTodo({ title: todoTitle, id: Date.now() });
   };
 
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input name="title" id="todoTitle" type="text" />
+      <input
+        onChange={handleTitleChange}
+        name="title"
+        id="todoTitle"
+        type="text"
+        value={todoTitle}
+      />
       <button type="submit">Add</button>
     </form>
   );
