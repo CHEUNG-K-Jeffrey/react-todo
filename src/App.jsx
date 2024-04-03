bc4d42a3e216b1b1586ef508f8295219875fc5b9
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-04-03 12:22:45 -0700

    Assign a default value
    Fixes crash when localStorage is empty
    
    Co-authored-by: Justice Ugochukwu Otuya <jotuya2@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 const useSemiPersistentState = () => {
   const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList"))
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
   );
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
