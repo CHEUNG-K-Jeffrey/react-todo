CHEUNG-K-Jeffrey/react-todo

commit bfb07c38d273b3810742383e377475f675284c30

Refactor to use pessimistic rendering
Co-authored-by: Justice Ugochukwu Otuya <jotuya2@gmail.com>

src/AddTodoForm.jsx
@@ -14,10 +14,8 @@ const AddTodoForm = (props) => {
   const handleAddTodo = async (event) => {
     event.preventDefault();
     try {
-      onAddTodo({ title: todoTitle, id: null });
-      const taskId = await onPostData(todoTitle);
-      onRemoveTodo(null);
-      onAddTodo({ title: todoTitle, id: taskId });
+      const newTodo = await onPostData(todoTitle);
+      onAddTodo(newTodo);
       setTodoTitle("");
     } catch (error) {
       onRemoveTodo(null);

commit 4facdb872030d454e27489d0418bb8fc583d121e

Fix regression caused by bfb07c3

src/AddTodoForm.jsx
@@ -15,7 +15,7 @@ const AddTodoForm = (props) => {
     event.preventDefault();
     try {
       const newTodo = await onPostData(todoTitle);
-      onAddTodo(newTodo);
+      onAddTodo({ title: newTodo.fields.title, id: newTodo.id });
       setTodoTitle("");
     } catch (error) {
       onRemoveTodo(null);