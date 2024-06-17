bec8cb1679d264b7b70c7f78fbbf998da48d237b
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-06-17 12:57:39 -0700

    Format code

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
       const data = await response.json();
 
       const todos: TtodoList = data.records
-.map((todo: { fields: { title: string }; id: string }) => {
+        .map((todo: { fields: { title: string }; id: string }) => {
           return { title: todo.fields.title, id: todo.id } as Ttodo;
         }) // Sort by descending order
         .sort((a: Ttodo, b: Ttodo) => {
